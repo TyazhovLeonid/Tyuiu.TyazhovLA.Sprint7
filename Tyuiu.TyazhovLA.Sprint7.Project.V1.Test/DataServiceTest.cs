14de87adf5dbeb557b853e38799c87146cb3cb9a
Tyuiu.TyazhovLA@outlook.com <99raindeath@gmail.com>
2024-12-11 23:02:57 +0500

    7

--- Tyuiu.TyazhovLA.Sprint7.Project.V1.Test/DataServiceTest.cs
+++ Tyuiu.TyazhovLA.Sprint7.Project.V1.Test/DataServiceTest.cs
@@ -1,11 +1,16 @@
+using Tyuiu.TyazhovLA.Sprint7.Project.V1.Lib;
 namespace Tyuiu.TyazhovLA.Sprint7.Project.V1.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckGetDataValid ()
         {
+            DataService ds = new DataService ();
+            var res = ds.GetData("C:\\Users\\myach\\OneDrive\\Рабочий стол\\test.csv");
+            string[,] wait = { {"А555АА777", "Toyota", "Розовый", "700" } };
+            CollectionAssert.AreEqual(wait,res);
         }
     }
 }
